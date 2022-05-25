5cd1a0e6cddcc028d16901abf237f367a367aefc
aburto22 <aburto22@gmail.com>
2022-05-25 18:52:08 +0200

    added disconnecting from sockets when unmounting App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Board from './components/Board';
 import Chat from './components/Chat';
-import './socket';
+import io from './socket';
 import styles from './App.module.scss';
 
-const App = () => (
-  <div className={styles.app}>
-    <Board />
-    <Chat />
-  </div>
-);
+const App = () => {
+  useEffect(() => () => {
+    io.disconnect();
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <Board />
+      <Chat />
+    </div>
+  );
+};
 
 export default App;
