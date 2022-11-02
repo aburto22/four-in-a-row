2824a21989e1fbfc551e8a1cc0ffc959f68ae209
aburto22 <aburto22@gmail.com>
2022-11-02 10:06:57 +0100

    Revert "create app dir with home page in there"
    This reverts commit 6a5f4a4ed5341ca8d7477a3a61cc057d9c454a0f.

--- apps/client/app/layout.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const App = styled.div`
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 100%;
-  grid-template-rows: 5rem 1fr;
-  padding: 1rem;
-`;
--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = nextConfig;
